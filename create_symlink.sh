b6d697c10e0ffa21b506435a7fb41a7c3a8f1cff
Thomas <barrepitousthomas@gmail.com>
2026-03-11 17:53:56 +0100

    Restructuration for agents folder

--- create_symlink.sh
+++ create_symlink.sh
@@ -2,6 +2,10 @@
 
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Claude CLI installation folder to .config
 ln -sfn ~/.config/.claude ~/.claude
+
+# Skills to .config (which are normally in a separate .agents folder in root)
+ln -s ~/.config/.claude/agents ~/.agents
 
 echo "Symlink created: ~/.claude -> ~/.config/.claude"
